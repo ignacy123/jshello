75bf04bd73c0a582039288a0c74e469d0bf1ffaa
ignacy <ignacy.buczek@onet.pl>
2017-03-08 15:18:56 +0100

    darft

--- js/script.js
+++ js/script.js
@@ -7,27 +7,21 @@
         }
 
         var listElem = document.getElementsByClassName("main");
-        console.log(listElem);
+        console.log(1,listElem);
         for (var i = 0; i < listElem.length; i++) {
             listElem[i].addEventListener("click", function (e) {
+                e.stopPropagation();
                 console.log(this, e);
-                var child = this.children[0]
-                child.style.display = "block";
+                var child = this.children[0];
                 child.className += " open";
 
             })
         }
         window.addEventListener("click", function () {
             var listOpen = document.getElementsByClassName("open");
-
+            console.log(2,listOpen);
             for (var i = 0; i < listOpen.length; i++) {
-                listOpen[i].addEventListener("click", function (e) {
-                    console.log(this, e);
-                    var child = this.children[0]
-                    child.style.display = "none";
-                    child.className = removeClass(child.className, "open");
-
-                })
+                listOpen[i].className = "";
             }
         })
     }//TODO: window add event listener nie działa,
