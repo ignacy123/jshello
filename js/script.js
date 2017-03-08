6e158b51f7b7d47b9926de908b1dcd440de38d16
Tomasz Brudzinski <tomasz.brudzinski@msleopard.com>
2017-03-08 15:32:07 +0100

    domkniecie JS

--- js/script.js
+++ js/script.js
@@ -24,5 +24,39 @@
                 listOpen[i].className = "";
             }
         })
-    }//TODO: window add event listener nie działa,
+    }
+
+	// DOMKNIĘCIE JS - zmiennie liczba1 i liczba2 stają się prywatne
+    // funkcja ma zawsze dostęp do kontekstu, w którym została utworzona, dlatego funkcje getLiczbaX oraz setLiczbaX
+    // mają dostęp do zmienny liczba1 i liczba 2 będących polami obiektu
+
+	var testowaKlasa = function(liczba){
+
+		var liczba1 = liczba;
+		var liczba2 = 5;
+
+		return {
+			jakisString: "oidsmfosidvm",
+			getLiczba1: function(){
+				return liczba1;
+			},
+			getLiczba2: function(){
+				return liczba2;
+			},
+            setLiczba1: function(nowaLiczba){
+			    liczba1 = nowaLiczba;
+            }
+		}
+	}
+
+	var testowyObiekt = testowaKlasa(1111119);
+
+	console.log(testowyObiekt.getLiczba1());
+	console.log(testowyObiekt.getLiczba2());
+	console.log(testowyObiekt.jakisString);
+
+	testowyObiekt.setLiczba1(222222);
+
+	console.log(testowyObiekt.getLiczba1());
+
 })()
